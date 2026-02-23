d1d63c1f78f528e25a4e7ab9619ce2abfdd8acb6
Sheikh Muhammad Riyadh Hasan <61405428+sheikh-riyadh@users.noreply.github.com>
2026-02-23 14:23:32 +0600

    Refactor AddTest component by removing invoice input
    Removed invoice state and input field for invoice entry.

--- src/components/pages/DueSample/AddTest.jsx
+++ src/components/pages/DueSample/AddTest.jsx
@@ -6,33 +6,15 @@ import Input from "../../common/Input";
 
 const AddTest = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [invoice, setInvoice] = useState();
 
   return (
     <>
-      <div className="flex items-center gap-5">
-        <Input
-          maxLength={9}
-          minLength={9}
-          placeholder="Add..."
-          onChange={(e) => {
-            const value = e.target.value;
-            setInvoice(value);
-            if (value.length === 9) {
-              setIsModalOpen(true);
-            } else {
-              setIsModalOpen(false);
-            }
-          }}
-        />
-
         <Button
           onClick={() => setIsModalOpen((prev) => !prev)}
           className="w-36"
         >
           Add 2hrs
         </Button>
-      </div>
 
       {isModalOpen && (
         <Modal
